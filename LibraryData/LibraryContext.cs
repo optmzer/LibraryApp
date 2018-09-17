ac22a478d3f3d40486a52294382871d912c48727
Alexander Frolov <big_air2409@mail.ru>
2018-09-17 18:23:37 +1200

    Added definition for database.

---
+++ LibraryData/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LibraryData
+{
+    public class LibraryContext: DbContext
+    {
+        //Passing options from LibraryContext constructor to base constructor
+        public LibraryContext(DbContextOptions options): base(options)  { }
+
+        public DbSet<Patron> Patrons { get; set; }
+
+    }
+}
