a1290843ea7083ef91fa2c547d9f2a7d57bdc745
Alexander Frolov <big_air2409@mail.ru>
2018-09-22 21:45:19 +1200

    Added Detail action to CatalogController and corresponding View.

--- LibraryApp/Controllers/CatologController.cs
+++ LibraryApp/Controllers/CatologController.cs
@@ -38,5 +38,30 @@ namespace LibraryApp.Controllers
             return View(model);
         }
 
+        /**
+         * localhost:48777/Catalog/Detail/{int: Id}
+         */
+        public IActionResult Detail(int id)
+        {
+            var asset = _assets.GetById(id);
+
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _assets.GetAuthorOrDirector(id),
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _assets.GetDeweyIndex(id),
+                ISBN = _assets.GetIsbn(id),
+            };
+
+            return View(model);
+        }
+
+
     }
 }
