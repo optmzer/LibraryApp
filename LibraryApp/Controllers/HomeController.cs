8cc1a4651138a1c8f34eb1d2610693756685e1d4
Alexander Frolov <big_air2409@mail.ru>
2018-10-07 20:16:19 +1300

    Added landing page. Edited _Layouts and Home/Index. Works OK.

--- LibraryApp/Controllers/HomeController.cs
+++ LibraryApp/Controllers/HomeController.cs
@@ -11,24 +11,7 @@ namespace LibraryApp.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
